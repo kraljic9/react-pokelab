bbea9643e16361fe7d89eb8c07b0e06c343d23e7
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-04 15:38:36 +0100

    Added use effect and display for pokemon list

--- pokelab-project/src/pages/PokemonList.jsx
+++ pokelab-project/src/pages/PokemonList.jsx
@@ -1,5 +1,36 @@
+import { useContext, useEffect, useState } from "react";
+import { PokemonContext } from "../context/PokemonContext";
+import { Link } from "react-router-dom";
+
 function PokemonList() {
-  return <h1>Pokemon List</h1>;
+  const { fetchPokemonList, loading, error, pokemonList } =
+    useContext(PokemonContext);
+
+  useEffect(() => {
+    fetchPokemonList(20, 0);
+
+    return () => console.log("Stopped fetching pokemon list");
+  }, []);
+
+  console.log(pokemonList);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{`Error accured: ${error}`}</p>;
+
+  return (
+    <>
+      <h1>Pokemon list</h1>
+
+      <div className="pokemon-grid">
+        {pokemonList.map((pokemon) => (
+          <li key={pokemon.name}>
+            <Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
+          </li>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default PokemonList;
