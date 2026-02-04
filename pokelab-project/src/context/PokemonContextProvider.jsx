bc203cd8596de3488f16dc0b0bb55839efd2729c
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-04 15:35:41 +0100

    Added context for fetching pokemon list, individual pokemon, error and loading

--- pokelab-project/src/context/PokemonContextProvider.jsx
+++ pokelab-project/src/context/PokemonContextProvider.jsx
@@ -1,5 +1,57 @@
-function PokemonContextProvider() {
-  return <h1>Pokemon Context Provider</h1>;
+import { useState } from "react";
+import { PokemonContext } from "./PokemonContext";
+
+function PokemonContextProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [pokemonList, setPokemonList] = useState(null);
+
+  async function fetchPokemonList(limit, offset) {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`,
+      );
+
+      if (!response.ok)
+        throw new Error("Error accured while fetching pokemon list");
+
+      const data = await response.json();
+
+      setPokemonList(data.results);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function fetchPokemon(name) {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${name}/`,
+      );
+
+      if (!response.ok) throw new Error("Error accured while fetching pokemon");
+
+      const data = await response.json();
+
+      return data;
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <PokemonContext.Provider
+      value={{ loading, error, fetchPokemon, fetchPokemonList, pokemonList }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  );
 }
 
 export default PokemonContextProvider;
