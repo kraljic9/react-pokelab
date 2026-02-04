5693a342068cf048690239a4fd4fcae3cd98e80f
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-04 16:14:28 +0100

    Fetcher data from url param and added guards for ui

--- pokelab-project/src/components/PokemonCard.jsx
+++ pokelab-project/src/components/PokemonCard.jsx
@@ -1,5 +1,43 @@
-function PokemonCard() {
-  return <h1>Pokemon Card</h1>;
+import { useEffect, useState } from "react";
+
+function PokemonCard({ url }) {
+  const [pokemonData, setPokemonData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok)
+          throw new Error("Error accured while fetching pokemon data");
+
+        const data = await response.json();
+
+        console.log(data);
+
+        setPokemonData(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPokemon();
+
+    return () => console.log("Error accured while fetching pokemon data");
+  }, []);
+
+  console.log(pokemonData);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error accured: {error}</p>;
+
+  return <h1>{pokemonData.name}</h1>;
 }
 
 export default PokemonCard;
