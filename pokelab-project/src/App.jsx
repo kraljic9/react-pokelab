1e5d91b664a3c5c92de55b30b662a5b317860ea2
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-04 14:31:30 +0100

    Added browser routing wrapper and added paths for pages

--- pokelab-project/src/App.jsx
+++ pokelab-project/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Home";
+import PokemonList from "./pages/PokemonList";
+import Pokemon from "./pages/Pokemon";
+import NotFound from "./NotFound";
 
 function App() {
-  return;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon" element={<PokemonList />} />
+          <Route path="/pokemon/:name" element={<Pokemon />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
