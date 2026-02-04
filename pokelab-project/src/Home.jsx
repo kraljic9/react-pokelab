930c8e610a34a867e5fad7d06abdae2a6a29b966
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-04 16:55:37 +0100

    Added buttons to switch pokemon

--- pokelab-project/src/Home.jsx
+++ pokelab-project/src/Home.jsx
@@ -1,5 +1,20 @@
+import { useContext } from "react";
+import PokemonList from "./pages/PokemonList";
+import { PokemonContext } from "./context/PokemonContext";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  const { setOffset } = useContext(PokemonContext);
+
+  return (
+    <>
+      <PokemonList />
+
+      <button onClick={() => setOffset((prev) => prev - 20)}>
+        Previous Page
+      </button>
+      <button onClick={() => setOffset((prev) => prev + 20)}>Next Page</button>
+    </>
+  );
 }
 
 export default Home;
