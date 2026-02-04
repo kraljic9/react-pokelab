ceb76f0155b69d84c5e7611ea59dcf8032db984d
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-04 15:33:45 +0100

    Added context provider wrap

--- pokelab-project/src/App.jsx
+++ pokelab-project/src/App.jsx
@@ -5,19 +5,22 @@ import PokemonList from "./pages/PokemonList";
 import Pokemon from "./pages/Pokemon";
 import NotFound from "./NotFound";
 import NavBar from "./components/NavBar";
+import PokemonContextProvider from "./context/PokemonContextProvider";
 
 function App() {
   return (
     <>
       <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pokemon" element={<PokemonList />} />
-          <Route path="/pokemon/:name" element={<Pokemon />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <PokemonContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pokemon" element={<PokemonList />} />
+            <Route path="/pokemon/:name" element={<Pokemon />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </PokemonContextProvider>
     </>
   );
 }
