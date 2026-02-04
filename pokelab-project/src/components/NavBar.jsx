6c75ec096e1dbd693c874e73930a22374108402a
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-04 14:42:51 +0100

    Added navigation buttons

--- pokelab-project/src/components/NavBar.jsx
+++ pokelab-project/src/components/NavBar.jsx
@@ -2,7 +2,14 @@ function NavBar() {
   return (
     <>
       <nav className="NavBar">
-        <div className="Logo">PokeLab</div>
+        <div className="side-logo">PokeLab</div>
+
+        <div className="main-logo">PokeLab Main</div>
+
+        <div className="button-container">
+          <button className="home-btn">Home</button>
+          <button className="about">About</button>
+        </div>
       </nav>
     </>
   );
