b49737789ac2b06817e73cca9df9afa30b782e46
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-06 16:07:10 +0100

    Added images for the logos and alt text

--- pokelab-project/src/components/NavBar.jsx
+++ pokelab-project/src/components/NavBar.jsx
@@ -2,13 +2,25 @@ function NavBar() {
   return (
     <>
       <nav className="NavBar">
-        <div className="side-logo">PokeLab</div>
+        <div className="side-logo">
+          <img
+            src="/src/assets/pokelab-logomini.png"
+            alt="poke-lab mini logo"
+            className="mini-logo"
+          />
+        </div>
 
-        <div className="main-logo">PokeLab Main</div>
+        <div className="main-logo">
+          <img
+            src="/src/assets/pokelab-logomain.png"
+            alt=""
+            className="main-logo"
+          />
+        </div>
 
         <div className="button-container">
-          <button className="home-btn">Home</button>
-          <button className="about">About</button>
+          <button className="nav-btn">Home</button>
+          <button className="nav-btn">About</button>
         </div>
       </nav>
     </>
