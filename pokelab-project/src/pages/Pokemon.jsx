1b60d3f62efc61a18cf9bae332b68d3569789f95
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-09 15:18:14 +0100

    Put big image outside of container

--- pokelab-project/src/pages/Pokemon.jsx
+++ pokelab-project/src/pages/Pokemon.jsx
@@ -16,13 +16,15 @@ function Pokemon() {
 
   function fetchTypes() {
     return currentPokemon?.types.map((type) => (
-      <li key={type.type.name}>{type.type.name}</li>
+      <li key={type.type.name} className="current-pokemon-type">
+        {type.type.name}
+      </li>
     ));
   }
 
   function fetchStats() {
     return currentPokemon?.stats.map((stat) => (
-      <li key={stat.stat.name}>
+      <li key={stat.stat.name} className="current-pokemon-stats">
         {stat.stat.name}: {stat.base_stat}
       </li>
     ));
@@ -39,41 +41,48 @@ function Pokemon() {
   if (!currentPokemon) return null;
 
   return (
-    <div className="current-pokemon-container">
-      <div className="current-pokemon-box">
-        <img
-          src={`${currentPokemon?.sprites.front_default}`}
-          alt=""
-          className="current-pokemon-img"
-        />
-        <h1 className="current-pokemon-name">{currentPokemon?.name}</h1>
+    <>
+      <div className="current-pokemon-container">
+        <div className="current-pokemon-box">
+          <img
+            src={`${currentPokemon?.sprites.front_default}`}
+            alt=""
+            className="current-pokemon-img"
+          />
+          <h1 className="current-pokemon-name">{currentPokemon?.name}</h1>
+        </div>
+
+        <div className="pokemon-info-boxes">
+          <div className="pokemon-info-box box-one ">
+            <h3 className="current-pokemon-info">Info</h3>
+            <p className="current-pokemon-txt">
+              Height: {currentPokemon.height / 10}m
+            </p>
+            <p className="current-pokemon-txt">
+              Weight: {currentPokemon.weight / 10}kg
+            </p>
+            <p className="current-pokemon-txt">
+              Base XP: {currentPokemon.base_experience}xp
+            </p>
+          </div>
+
+          <div className="pokemon-info-box box-two">
+            <h3 className="current-pokemon-type-title">Type</h3>
+            <ul className="current-pokemon-types">{fetchTypes()}</ul>
+          </div>
+
+          <div className="pokemon-info-box box-three">
+            <h3 className="current-pokemon-stats-title">Stats</h3>
+            <ul className="current-pokemon-stats-list">{fetchStats()}</ul>
+          </div>
+        </div>
       </div>
-
-      <div className="pokemon-info-boxes">
-        <h3 className="current-pokemon-info">Info</h3>
-        <p className="current-pokemon-txt">
-          Height: {currentPokemon.height / 10}m
-        </p>
-        <p className="current-pokemon-txt">
-          Weight: {currentPokemon.weight / 10}kg
-        </p>
-        <p className="current-pokemon-txt">
-          Base XP: {currentPokemon.base_experience}xp
-        </p>
-
-        <h3 className="current-pokemon-type-title">Type</h3>
-        <ul className="current-pokemon-types">{fetchTypes()}</ul>
-
-        <h3 className="current-pokemon-stats-title">Stats</h3>
-        <ul className="current-pokemon-stats">{fetchStats()}</ul>
-      </div>
-
       <img
-        src={`${currentPokemon?.sprites.front_default}`}
+        src={`${currentPokemon?.sprites.front_shiny}`}
         alt=""
         className="current-pokemon-big-img"
       />
-    </div>
+    </>
   );
 }
 
