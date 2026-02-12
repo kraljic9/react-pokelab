54815806d25c179c67701a5961dd12385299fbd3
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-02-12 17:24:07 +0100

    Wrapped nav bar images in Link wrappers

--- pokelab-project/src/components/NavBar.jsx
+++ pokelab-project/src/components/NavBar.jsx
@@ -1,26 +1,46 @@
+import { useNavigate, Link } from "react-router-dom";
+
 function NavBar() {
+  let navigate = useNavigate();
+
+  function goHome() {
+    navigate("/");
+  }
+
+  function goAbout() {
+    navigate("/about");
+  }
+
   return (
     <>
       <nav className="NavBar">
         <div className="side-logo">
-          <img
-            src="/src/assets/pokelab-logomini.png"
-            alt="poke-lab mini logo"
-            className="mini-logo"
-          />
+          <Link to="/">
+            <img
+              src="/src/assets/pokelab-logomini.png"
+              alt="poke-lab mini logo"
+              className="mini-logo"
+            />
+          </Link>
         </div>
 
         <div className="main-logo">
-          <img
-            src="/src/assets/pokelab-logomain.png"
-            alt=""
-            className="main-logo"
-          />
+          <Link to="/">
+            <img
+              src="/src/assets/pokelab-logomain.png"
+              alt=""
+              className="main-logo"
+            />
+          </Link>
         </div>
 
         <div className="button-container">
-          <button className="nav-btn">Home</button>
-          <button className="nav-btn">About</button>
+          <button className="nav-btn" onClick={goHome}>
+            Home
+          </button>
+          <button className="nav-btn" onClick={goAbout}>
+            About
+          </button>
         </div>
       </nav>
     </>
